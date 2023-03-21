f93c55e115a0b5682962bee3e92453edb62d2604
Lawrence Naumov <prostolawr@gmail.com>
2023-03-21 18:51:13 +0800

    Fix my button

--- app/src/components/UI/MyButton.vue
+++ app/src/components/UI/MyButton.vue
@@ -1,7 +1,5 @@
 <template>
     <div class="field-btn">
-        <div class="liceum">Лицей №2</div>
-        <div class="adress">г.Иркутск, пер. Волконского, 7</div>
         <button class="btn">
         <slot>Выбрать</slot>
         </button>
@@ -15,12 +13,6 @@ export default {
 </script>
 
 <style scoped>
-.adress {
-    margin:10px 0;
-}
-.liceum {
-    font-size: 20px;
-}
 .btn {
     background: #6D9773;
     margin: 0;
